7bd13078069638ba2fb19259ea7b07cbd6ec73b0
isijolatosin <isijolatosin2210@gmail.com>
2022-02-25 00:25:14 -0500

    project folder changed

--- functions/server.js
+++ functions/server.js
@@ -66,7 +66,7 @@ app.use(notFound)
 app.use(errorHandlerMiddleware)
 
 // server running
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 const start = async () => {
 	try {
 		await connectDB(process.env.MONGO_URI)
