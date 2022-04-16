811258df99a877fcbd19af7f1da5519726e6b586
isijolatosin <isijolatosin2210@gmail.com>
2022-04-16 18:30:39 -0400

    modelest upload folder created for cloudinary in uploadsController func

--- server/controllers/uploadsController.js
+++ server/controllers/uploadsController.js
@@ -39,7 +39,7 @@ const uploadProductImage = async (req, res) => {
 		req.files.image.tempFilePath,
 		{
 			use_filename: true,
-			folder: 'pvg-upload',
+			folder: 'modelest-upload',
 		}
 	)
 	fs.unlinkSync(req.files.image.tempFilePath)
