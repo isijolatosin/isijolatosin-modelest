f03e5fa713c0e9846b7a1169e67edffe1969af99
isijolatosin <isijolatosin2210@gmail.com>
2022-04-16 18:35:06 -0400

    image upload removed from allinventory page

--- src/components/AllInventories.js
+++ src/components/AllInventories.js
@@ -9,7 +9,7 @@ function AllInventories() {
 	const [singleProduct, setSingleProduct] = React.useState()
 	const [postError, setPostError] = React.useState('')
 	const [isTrue, setIsTrue] = React.useState(false)
-	const [imageFile, setImageFile] = React.useState('')
+	// const [imageFile, setImageFile] = React.useState('')
 
 	const togglePassword = () => {
 		setIsTrue(!isTrue)
@@ -157,7 +157,7 @@ function AllInventories() {
 				availablelength: singleProduct.availablelength,
 				description: singleProduct.description,
 				sales: isTrue,
-				image: imageFile,
+				// image: imageFile,
 			}
 			if (
 				product.name === '' ||
@@ -181,27 +181,27 @@ function AllInventories() {
 		fetchProducts()
 	}
 
-	const uploadFile = async (e) => {
-		e.preventDefault()
-		const imageFile = e.target.files[0]
-		const formData = new FormData()
-		formData.append('image', imageFile)
-		try {
-			const {
-				data: {
-					image: { src },
-				},
-			} = await axios.post('/api/v1/products/uploads', formData, {
-				headers: {
-					'Content-Type': 'multipart/form-data',
-				},
-			})
-
-			setImageFile(src)
-		} catch (error) {
-			console.log(error)
-		}
-	}
+	// 	const uploadFile = async (e) => {
+	// 		e.preventDefault()
+	// 		const imageFile = e.target.files[0]
+	// 		const formData = new FormData()
+	// 		formData.append('image', imageFile)
+	// 		try {
+	// 			const {
+	// 				data: {
+	// 					image: { src },
+	// 				},
+	// 			} = await axios.post('/api/v1/products/uploads', formData, {
+	// 				headers: {
+	// 					'Content-Type': 'multipart/form-data',
+	// 				},
+	// 			})
+	//
+	// 			setImageFile(src)
+	// 		} catch (error) {
+	// 			console.log(error)
+	// 		}
+	// 	}
 
 	return (
 		<div>
@@ -302,7 +302,7 @@ function AllInventories() {
 								placeholder="Description..."
 								className="tw-rounded tw-mt-1 tw-block lg:tw-w-[50%] tw-mx-auto tw-w-[90%] tw-px-3 tw-py-2 tw-border-none tw-text-sm tw-shadow-xl tw-placeholder-gray-400 focus:tw-outline-none focus:tw-border-gray-200 focus:tw-ring-1 focus:tw-ring-gray-200 isabled:tw-bg-gray-50 disabled:tw-text-gray-500 disabled:tw-border-gray-200 disabled:tw-shadow-none invalid:tw-border-pink-500 invalid:tw-text-pink-600 focus:invalid:tw-border-pink-500 focus:invalid:tw-ring-pink-500 tw-outline-0 tw-mb-5 placeholder:tw-font-light placeholder:tw-text-xs tw-text-gray-700 tw-font-light"
 							/>
-							<div className="tw-text-red-800 tw-text-sm tw-mb-2 ">
+							{/* <div className="tw-text-red-800 tw-text-sm tw-mb-2 ">
 								<span>
 									Rename Image and substitute space with a dash ( e.g abc-def )
 								</span>
@@ -316,7 +316,7 @@ function AllInventories() {
 									type="file"
 									className="tw-block tw-w-full tw-text-sm tw-text-gray-500 file:tw-mr-4 file:tw-py-1 file:tw-px-4 file:tw-rounded file:tw-border-0 file:tw-text-sm file:tw-bg-gray-200 file:tw-text-violet-700 hover:file:tw-bg-violet-100 tw-ease-in tw-duration-300"
 								/>
-							</div>
+							</div> */}
 							<div className="tw-rounded tw-flex tw-flex-row tw-items-center tw-mt-5 tw-shadow-xl tw-p-2">
 								<label className="tw-mr-3 tw-text-gray-500 tw-text-sm">
 									Sales
