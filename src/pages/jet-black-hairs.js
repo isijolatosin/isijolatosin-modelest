6e0e58e5d65da9d79231e45d4fbd1c380efc872c
isijolatosin <isijolatosin2210@gmail.com>
2022-06-15 18:34:19 -0400

    shipping logic updated

--- src/pages/jet-black-hairs.js
+++ src/pages/jet-black-hairs.js
@@ -229,7 +229,7 @@ function JetBlackHair() {
 						</div>
 					)}
 					{singleProducts && (
-						<div className="single tw-absolute tw-z-30 tw-overflow-scroll tw-w-[100%] tw-h-[100%] tw-right-0 tw-left-0 tw-top-[130px] md:tw-top-[95px] md:tw-pt-5 tw-flex tw-flex-col tw-items-start tw-justify-center tw-bg-white">
+						<div className="single tw-absolute tw-z-30 tw-overflow-scroll tw-w-[100%] tw-h-[100%] tw-right-0 tw-left-0 tw-top-0 md:tw-top-[95px] md:tw-pt-5 tw-flex tw-flex-col tw-items-start tw-justify-center tw-bg-white">
 							<div className="md:tw-w-[80%] xl:tw-w-[70%] md:tw-mx-auto tw-w-[100%] tw-h-full tw-flex md:tw-flex-row tw-flex-col">
 								<div className="tw-w-[100%] md:tw-h-[500px] md:tw-w-[50%] tw-mx-auto md:tw-mr-10">
 									<img
