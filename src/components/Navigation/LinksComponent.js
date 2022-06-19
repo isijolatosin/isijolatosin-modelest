f51729e5184e0a77a9abc5371d33a1750570e048
isijolatosin <isijolatosin2210@gmail.com>
2022-06-19 04:46:33 -0400

    customer reviews added

--- src/components/Navigation/LinksComponent.js
+++ src/components/Navigation/LinksComponent.js
@@ -43,17 +43,18 @@ function LinksComponent() {
 		<div className="tw-grid tw-grid-cols-3 tw-w-full">
 			<div
 				onMouseOver={handleMousein1}
+				onMouseLeave={() => setList1(false)}
 				className="tw-w-full tw-relative tw-uppercase tw-font-bold tw-text-[10px]">
-				<div className="tw-flex tw-flex-row tw-justify-between tw-items-center hover:tw-cursor-pointer tw-border-r-[1px] tw-border-neutral-600 tw-pr-2 tw-mr-4">
+				<div className="tw-flex tw-flex-row tw-justify-between tw-items-center hover:tw-cursor-pointer tw-border-l-[1px] tw-border-neutral-600 tw-pl-2 tw-mr-4">
 					Indian - hair
 				</div>
 
 				{list1 && (
-					<div className="tw-absolute tw-top-[25px] md:tw-top-10 tw-left-[-10px] tw-flex tw-flex-col tw-justify-left tw-ease-in tw-duration-300">
+					<div className="tw-absolute tw-top-[25px] md:tw-top-4 tw-left-[-10px] tw-flex tw-flex-col tw-justify-left tw-ease-in tw-duration-300">
 						{subLinks.map((item) => (
 							<Link
 								to={item.route}
-								className="tw-px-3 tw-rounded-sm tw-pl-5 md:tw-w-[250px] tw-w-[200px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 hover:tw-text-zinc-400 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500"
+								className="tw-px-3 tw-rounded-sm md:tw-w-[250px] tw-w-[200px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 tw-text-neutral-800 hover:tw-text-neutral-500 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500"
 								key={item.id}>
 								{item.name}
 							</Link>
@@ -63,27 +64,28 @@ function LinksComponent() {
 			</div>
 			<div
 				onMouseOver={handleMousein2}
+				onMouseLeave={() => setList2(false)}
 				className="tw-w-full tw-relative tw-uppercase tw-font-bold tw-text-[10px]">
-				<div className="tw-flex tw-flex-row tw-justify-between tw-items-center hover:tw-cursor-pointer tw-border-r-[1px] tw-border-neutral-600 tw-pr-2 tw-mr-4">
+				<div className="tw-flex tw-flex-row tw-justify-between tw-items-center hover:tw-cursor-pointer tw-border-l-[1px] tw-border-neutral-600 tw-pl-2 tw-mr-4">
 					Vietnamese - hair
 				</div>
 
 				{list2 && (
-					<div className="tw-absolute tw-top-[25px] md:tw-top-10 tw-left-[-50px] md:tw-left-[-18px] tw-flex tw-flex-col tw-justify-left tw-ease-in tw-duration-300">
-						<p className="tw-text-center md:tw-text-left tw-px-3 tw-rounded-sm tw-pl-5 md:tw-w-[250px] tw-w-[200px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 hover:tw-text-zinc-400 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500">
+					<div className="tw-absolute tw-top-[25px] md:tw-top-4 tw-left-[-50px] md:tw-left-[-18px] tw-flex tw-flex-col tw-justify-left tw-ease-in tw-duration-300">
+						<p className="tw-text-center md:tw-text-left tw-px-3 tw-rounded-sm tw-pl-5 md:tw-w-[250px] tw-w-[200px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 tw-text-neutral-800 hover:tw-text-neutral-500 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500">
 							coming soon...
 						</p>
-						<p className="tw-text-center md:tw-text-left tw-px-3 tw-rounded-sm tw-pl-5 md:tw-w-[250px] tw-w-[200px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 hover:tw-text-zinc-400 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500">
+						<p className="tw-text-center md:tw-text-left tw-px-3 tw-rounded-sm tw-pl-5 md:tw-w-[250px] tw-w-[200px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 tw-text-neutral-800 hover:tw-text-neutral-500 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500">
 							coming soon...
 						</p>
-						<p className="tw-text-center md:tw-text-left tw-px-3 tw-rounded-sm tw-pl-5 md:tw-w-[250px] tw-w-[200px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 hover:tw-text-zinc-400 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500">
+						<p className="tw-text-center md:tw-text-left tw-px-3 tw-rounded-sm tw-pl-5 md:tw-w-[250px] tw-w-[200px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 tw-text-neutral-800 hover:tw-text-neutral-500 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500">
 							coming soon...
 						</p>
 
 						{/* {subLinks.map((item) => (
 							<Link
 								to={item.route}
-								className="tw-text-center md:tw-text-left tw-px-3 tw-rounded-sm tw-pl-5 tw-w-[250px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 hover:tw-text-zinc-400 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500"
+								className="tw-text-center md:tw-text-left tw-px-3 tw-rounded-sm tw-pl-5 tw-w-[250px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 tw-text-neutral-800 hover:tw-text-neutral-500 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500"
 								key={item.id}>
 								{item.name}
 							</Link>
@@ -93,26 +95,27 @@ function LinksComponent() {
 			</div>
 			<div
 				onMouseOver={handleMousein3}
+				onMouseLeave={() => setList3(false)}
 				className="tw-w-full tw-relative tw-uppercase tw-font-bold tw-text-[10px]">
-				<div className="tw-flex tw-flex-row tw-justify-between tw-items-center hover:tw-cursor-pointer tw-pr-2 tw-mr-4">
+				<div className="tw-flex tw-flex-row tw-justify-between tw-items-center hover:tw-cursor-pointer tw-border-l-[1px] tw-border-neutral-600 tw-pl-2 tw-mr-4">
 					Brazillian - hair
 				</div>
 
 				{list3 && (
-					<div className="tw-absolute tw-top-[25px] md:tw-top-10 tw-right-[-10px] md:tw-right-0 md:tw-left-[-18px] tw-flex tw-flex-col tw-justify-left tw-ease-in tw-duration-300">
-						<p className="tw-text-right md:tw-text-left tw-px-3 tw-rounded-sm tw-pl-5 md:tw-w-[250px] tw-w-[200px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 hover:tw-text-zinc-400 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500">
+					<div className="tw-absolute tw-top-[25px] md:tw-top-4 tw-right-[-10px] md:tw-right-0 md:tw-left-[-18px] tw-flex tw-flex-col tw-justify-left tw-ease-in tw-duration-300">
+						<p className="tw-text-right md:tw-text-left tw-px-3 tw-rounded-sm tw-pl-5 md:tw-w-[250px] tw-w-[200px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 tw-text-neutral-800 hover:tw-text-neutral-500 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500">
 							coming soon...
 						</p>
-						<p className="tw-text-right md:tw-text-left tw-px-3 tw-rounded-sm tw-pl-5 md:tw-w-[250px] tw-w-[200px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 hover:tw-text-zinc-400 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500">
+						<p className="tw-text-right md:tw-text-left tw-px-3 tw-rounded-sm tw-pl-5 md:tw-w-[250px] tw-w-[200px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 tw-text-neutral-800 hover:tw-text-neutral-500 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500">
 							coming soon...
 						</p>
-						<p className="tw-text-right md:tw-text-left tw-px-3 tw-rounded-sm tw-pl-5 md:tw-w-[250px] tw-w-[200px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 hover:tw-text-zinc-400 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500">
+						<p className="tw-text-right md:tw-text-left tw-px-3 tw-rounded-sm tw-pl-5 md:tw-w-[250px] tw-w-[200px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 tw-text-neutral-800 hover:tw-text-neutral-500 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500">
 							coming soon...
 						</p>
 						{/* {subLinks.map((item) => (
 							<Link
 								to={item.route}
-								className="tw-text-right md:tw-text-left tw-px-3 tw-rounded-sm tw-pl-5 tw-w-[250px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 hover:tw-text-zinc-400 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500"
+								className="tw-text-right md:tw-text-left tw-px-3 tw-rounded-sm tw-pl-5 tw-w-[250px] tw-bg-neutral-400 tw-mb-[1px] tw-text-[12px] tw-p-5 tw-text-neutral-800 hover:tw-text-neutral-500 hover:tw-cursor-pointer tw-ease-in-out tw-duration-500"
 								key={item.id}>
 								{item.name}
 							</Link>
--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -4,7 +4,7 @@ import Nav from '../Navigation/Nav'
 function Layout({ children, allProducts }) {
 	return (
 		<div>
-			<div className="bg-blur2 tw-fixed tw-top-0 tw-left-0 tw-right-0 tw-z-20 tw-max-w-screen ">
+			<div className="tw-bg-neutral-400 tw-fixed tw-top-0 tw-left-0 tw-right-0 tw-z-20 tw-max-w-screen ">
 				<Nav allProducts={allProducts} />
 			</div>
 			<div className="tw-mt-2">{children}</div>
