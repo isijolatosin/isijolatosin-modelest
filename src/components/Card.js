11ffb51debeb0cc5d3b159533dd143094a439dcc
isijolatosin <isijolatosin2210@gmail.com>
2022-06-26 00:58:18 -0400

    sales input added to admin portal

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import axios from 'axios'
-import { GrCheckmark } from 'react-icons/gr'
 import truncate from '../utils/truncate'
 
 function Card({ product, setSingleproducts, scrollToTop, sales }) {
